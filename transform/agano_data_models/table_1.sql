ce6920089d0329a5b68d0761900cab90b53ed63a
Christian Agano <aganochristian@gmail.com>
2024-05-05 21:31:59 +0000

    commit

--- transform/agano_data_models/table_1.sql
+++ transform/agano_data_models/table_1.sql
@@ -1,72 +1,68 @@
-{{
-    config(
-        materialized='view'
+{{ config(materialized="table") }}
+
+
+with
+    ord_1 as (
+        select
+            row_number() over (order by date_local asc) as order_index,
+            rdbms_id,
+            country_name,
+            date_local,
+            vendor_id,
+            customer_id,
+            gmv_local,
+            is_voucher_used,
+            is_successful_order,
+            product_id
+        from `foodpanda-422109.cs_1_2.orders`
+    ),
+
+    ord_2 as (
+        select
+            order_index,
+            rdbms_id,
+            country_name,
+            date_local,
+            vendor_id,
+            customer_id,
+            gmv_local,
+            is_voucher_used,
+            is_successful_order,
+            trim(product_id) as product_id
+        from ord_1, unnest(split(product_id, ',')) as product_id
+    ),
+
+    ord_3 as (
+        select *, count(*) as product_quantity
+        from ord_2
+        group by
+            order_index,
+            rdbms_id,
+            country_name,
+            date_local,
+            vendor_id,
+            customer_id,
+            gmv_local,
+            is_voucher_used,
+            is_successful_order,
+            product_id
+    ),
+
+    final_table as (
+        select *
+        from ord_3 o
+        left join `foodpanda-422109.cs_1_2.vendors` v on v.id = o.vendor_id
+        left join
+            `foodpanda-422109.cs_1_2.products` p on cast(p.id as string) = o.product_id
+        left join `foodpanda-422109.cs_1_2.customers` c on c.id = o.customer_id
+    ),
+
+    successful_orders_per_day as (
+        select date_local, count(distinct order_index) as successful_orders
+        from final_table
+        where is_successful_order = true
+        group by date_local
     )
-}}
 
-
-with ord_1 as (
-    SELECT 
-        row_number() over (order by date_local asc) as order_index,
-        rdbms_id,
-        country_name,
-        date_local,
-        vendor_id,
-        customer_id,
-        gmv_local,
-        is_voucher_used,
-        is_successful_order,
-        product_id
-    FROM `foodpanda-422109.cs_1_2.orders`
-),
-
-ord_2 as (
-    SELECT 
-        order_index,
-        rdbms_id,
-        country_name,
-        date_local,
-        vendor_id,
-        customer_id,
-        gmv_local,
-        is_voucher_used,
-        is_successful_order,
-        TRIM(product_id) AS product_id
-    FROM ord_1,
-    UNNEST(SPLIT(product_id, ',')) AS product_id
-),
-
-ord_3 as (
-    SELECT 
-        *,
-        COUNT(*) as product_quantity
-    FROM ord_2
-    GROUP BY 
-        order_index,
-        rdbms_id,
-        country_name,
-        date_local,
-        vendor_id,
-        customer_id,
-        gmv_local,
-        is_voucher_used,
-        is_successful_order, 
-        product_id
-),
-
-final_table as (
-    SELECT *
-    FROM ord_3 o
-    LEFT JOIN `foodpanda-422109.cs_1_2.vendors` v ON v.id = o.vendor_id
-    LEFT JOIN `foodpanda-422109.cs_1_2.products` p ON cast(p.id as string) = o.product_id
-    LEFT JOIN `foodpanda-422109.cs_1_2.customers` c ON c.id = o.customer_id
-),
-
-successful_orders_per_day as (
-    SELECT date_local, COUNT(DISTINCT order_index) as successful_orders
-    FROM final_table 
-    WHERE is_successful_order=true
-    GROUP BY date_local
-)
-
-SELECT * FROM successful_orders_per_day
+select *
+from successful_orders_per_day
